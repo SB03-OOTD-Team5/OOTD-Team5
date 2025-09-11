d2379aa24dfe1df8aa6908493a0ad28ad38b7ad8
Kangmoongu <sprtms5325@naver.com>
2025-09-11 13:49:35 +0900

    feat: 기초 파일 설정 추가

---
+++ src/main/java/com/sprint/ootd5team/Ootd5TeamApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.ootd5team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ootd5TeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ootd5TeamApplication.class, args);
+    }
+
+}
