25cf63c014a228fc5e4f54df2608e8b9555ea800
Leichtstar <leichtnidel@gmail.com>
2025-09-12 10:40:05 +0900

    feat: 옷 속성 등록,조회 메서드 선언 - CODEITSB03-67, CODEITSB03-47

--- src/main/java/com/sprint/ootd5team/domain/clothattribute/service/ClothAttributeService.java
+++ src/main/java/com/sprint/ootd5team/domain/clothattribute/service/ClothAttributeService.java
@@ -1,5 +1,11 @@
 package com.sprint.ootd5team.domain.clothattribute.service;
 
-public interface ClothAttributeService {
+import com.sprint.ootd5team.domain.clothattribute.dto.ClothesAttributeDefCreateRequest;
+import com.sprint.ootd5team.domain.clothattribute.entity.ClothAttribute;
+import java.util.List;
 
+public interface ClothAttributeService {
+	ClothAttribute create(ClothesAttributeDefCreateRequest request);
+	List<ClothAttribute> getClothAttributes();
+	ClothAttribute getClothAttributeById(Long id);
 }
