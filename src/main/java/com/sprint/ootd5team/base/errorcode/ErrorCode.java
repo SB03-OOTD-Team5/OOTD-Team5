e6aaf37e4dd9c59123f305864dc2bdeb7a225e05
Kangmoongu <sprtms5325@naver.com>
2025-09-12 16:37:50 +0900

    feat: ErrorCode와 기본 Exception 정의 및 UserException 생성

---
+++ src/main/java/com/sprint/ootd5team/base/errorcode/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.sprint.ootd5team.base.errorcode;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    // Security 관련 에러 코드
+    UNAUTHORIZED("권한이 없는 사용자 입니다."),
+    INVALID_USER_CREDENTIALS("잘못된 사용자 인증 정보입니다."),
+
+    // User 관련 에러코드
+    USER_NOT_FOUND("존재하지 않는 사용자 입니다.");
+
+
+
+
+
+    private final String message;
+
+    ErrorCode(String message) {
+        this.message = message;
+    }
+}
