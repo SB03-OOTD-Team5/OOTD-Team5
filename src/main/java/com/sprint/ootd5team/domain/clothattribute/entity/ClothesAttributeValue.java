77d4e09b98cee4868c407dac97fcaa947a931896
Leichtstar <leichtnidel@gmail.com>
2025-09-15 14:09:55 +0900

    fix: index "ix_cav_cloth_attr"의 컬럼 "attributes_id" 오타수정

--- src/main/java/com/sprint/ootd5team/domain/clothattribute/entity/ClothesAttributeValue.java
+++ src/main/java/com/sprint/ootd5team/domain/clothattribute/entity/ClothesAttributeValue.java
@@ -30,8 +30,8 @@ import lombok.Setter;
 		)
 	},
 	indexes = {
-		@Index(name = "ix_cav_cloth_attr", columnList = "clothes_id, attributes_id"),
-		@Index(name = "ix_cav_attr_defvalue", columnList = "attributes_id, def_value")
+		@Index(name = "ix_cav_cloth_attr", columnList = "clothes_id, attribute_id"),
+		@Index(name = "ix_cav_attr_defvalue", columnList = "attribute_id, def_value")
 	}
 )
 @Getter
