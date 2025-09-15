b4bb91fa92e83dff1dfff4f4ff9471c21414af1b
hyohyo-zz <akbkck8101@gmail.com>
2025-09-15 14:29:46 +0900

    feat: 옷 등록 기능 로컬 업로드 기능 구현
    - CODEITSB03-87

---
+++ src/main/java/com/sprint/ootd5team/base/storage/FileStorage.java
@@ -0,0 +1,11 @@
+package com.sprint.ootd5team.base.storage;
+
+import java.io.InputStream;
+
+public interface FileStorage {
+
+    String upload(String filename, InputStream input);
+    String download(String path);
+    void delete(String path);
+
+}
---
+++ src/main/java/com/sprint/ootd5team/base/storage/LocalFileStorage.java
@@ -0,0 +1,50 @@
+package com.sprint.ootd5team.base.storage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "ootd.storage.type", havingValue = "local")
+public class LocalFileStorage implements FileStorage {
+
+    private final Path root = Paths.get("uploads");
+
+    public LocalFileStorage() {
+        try {
+            Files.createDirectories(root);
+        } catch (IOException e) {
+            throw new RuntimeException("업로드 폴더 생성 실패", e);
+        }
+    }
+
+    @Override
+    public String upload(String filename, InputStream input) {
+        try {
+            Path target = root.resolve(filename);
+            Files.copy(input, target, StandardCopyOption.REPLACE_EXISTING);
+            return target.toAbsolutePath().toString();
+        } catch (IOException e) {
+            throw new RuntimeException("로컬 업로드 실패", e);
+        }
+    }
+
+    @Override
+    public String download(String path) {
+        return Paths.get(path).toAbsolutePath().toUri().toString();
+    }
+
+    @Override
+    public void delete(String path) {
+        try {
+            Files.deleteIfExists(Paths.get(path));
+        } catch (IOException e) {
+            throw new RuntimeException("로컬 삭제 실패", e);
+        }
+    }
+}
