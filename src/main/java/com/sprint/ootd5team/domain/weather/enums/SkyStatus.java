fa7d03dfd2cb72a659bca2bae285499ed4160038
yeokyeong <yeokyeong1004@gmail.com>
2025-09-16 15:47:57 +0900

    fix: kma 카테고리 추가

--- src/main/java/com/sprint/ootd5team/domain/weather/enums/SkyStatus.java
+++ src/main/java/com/sprint/ootd5team/domain/weather/enums/SkyStatus.java
@@ -1,7 +1,7 @@
 package com.sprint.ootd5team.domain.weather.enums;
 
 public enum SkyStatus {
-    CLEAR,
-    MOSTLY_CLOUDY,
-    CLOUDY
+    CLEAR, // 0~5
+    MOSTLY_CLOUDY, // 6~8
+    CLOUDY // 9~10
 }
