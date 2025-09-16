19e33386c3d3f3a23bb4b5c4af885d734d324d00
Im-Ubin <yuvin3493@gmail.com>
2025-09-16 16:51:16 +0900

    feat: 커스텀 예외 처리 구현
    - ErrorCode status 필드 추가
    - ErrorResponse 사용하지 않는 import문 제거
    - GlobalExceptionHandler 핸들러 추가
    - FeedNotFoundException
    - InvalidSortOptionException

--- src/main/java/com/sprint/ootd5team/domain/feed/exception/FeedNotFoundException.java
+++ src/main/java/com/sprint/ootd5team/domain/feed/exception/FeedNotFoundException.java
@@ -1,15 +1,18 @@
 package com.sprint.ootd5team.domain.feed.exception;
 
+import com.sprint.ootd5team.base.errorcode.ErrorCode;
+import com.sprint.ootd5team.base.exception.OotdException;
 import java.util.UUID;
 import lombok.Getter;
 
 @Getter
-public class FeedNotFoundException extends RuntimeException {
+public class FeedNotFoundException extends OotdException {
 
     private final UUID feedId;
 
     public FeedNotFoundException(UUID feedId) {
-        super("존재하지 않는 피드입니다. feedId: " + feedId);
+        super(ErrorCode.FEED_NOT_FOUND);
         this.feedId = feedId;
+        this.addDetail("feedId", feedId);
     }
 }
--- src/main/java/com/sprint/ootd5team/domain/feed/exception/InvalidSortOptionException.java
+++ src/main/java/com/sprint/ootd5team/domain/feed/exception/InvalidSortOptionException.java
@@ -1,14 +1,17 @@
 package com.sprint.ootd5team.domain.feed.exception;
 
+import com.sprint.ootd5team.base.errorcode.ErrorCode;
+import com.sprint.ootd5team.base.exception.OotdException;
 import lombok.Getter;
 
 @Getter
-public class InvalidSortOptionException extends RuntimeException {
+public class InvalidSortOptionException extends OotdException {
 
     private final String sortBy;
 
     public InvalidSortOptionException(String sortBy) {
-        super("유효하지 않은 정렬 옵션입니다. sortBy: " + sortBy);
+        super(ErrorCode.INVALID_SORT_OPTION);
         this.sortBy = sortBy;
+        this.addDetail("sortBy", sortBy);
     }
 }
