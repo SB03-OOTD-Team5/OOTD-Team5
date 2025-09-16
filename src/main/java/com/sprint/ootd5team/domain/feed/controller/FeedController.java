403408821a3d90bd5874998972e1a7c3c8fd30fc
Im-Ubin <yuvin3493@gmail.com>
2025-09-16 14:11:07 +0900

    feat: controller 메서드 시그니처 수정
    - FeedApi
    - FeedController

--- src/main/java/com/sprint/ootd5team/domain/feed/controller/FeedController.java
+++ src/main/java/com/sprint/ootd5team/domain/feed/controller/FeedController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -30,10 +29,9 @@ public class FeedController implements FeedApi {
     public ResponseEntity<FeedDtoCursorResponse> getFeeds(
         @Valid @ModelAttribute FeedListRequest feedListRequest,
         @RequestParam(required = false) UUID currentUserId
-//        @AuthenticationPrincipal CustomUserDetails user
+//        @AuthenticationPrincipal OotdUserDetails user
     ) {
-//        UUID currentUserId = user.getId();
-
+//        UUID userid = user.getUserDto().id();
         FeedDtoCursorResponse feeds = feedService.getFeeds(feedListRequest, currentUserId);
         return ResponseEntity
             .status(HttpStatus.OK)
--- src/main/java/com/sprint/ootd5team/domain/feed/controller/api/FeedApi.java
+++ src/main/java/com/sprint/ootd5team/domain/feed/controller/api/FeedApi.java
@@ -1,5 +1,6 @@
 package com.sprint.ootd5team.domain.feed.controller.api;
 
+import com.sprint.ootd5team.base.exception.ErrorResponse;
 import com.sprint.ootd5team.domain.feed.dto.request.FeedListRequest;
 import com.sprint.ootd5team.domain.feed.dto.response.FeedDtoCursorResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,9 +20,10 @@ public interface FeedApi {
             content = @Content(schema = @Schema(implementation = FeedDtoCursorResponse.class))
         ),
         @ApiResponse(
-            responseCode = "400", description = "피드 목록 조회 실패"
-//            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            responseCode = "400", description = "피드 목록 조회 실패",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
         )
     })
     ResponseEntity<FeedDtoCursorResponse> getFeeds(FeedListRequest feedListRequest, UUID currentUserId);
+//    ResponseEntity<FeedDtoCursorResponse> getFeeds(FeedListRequest feedListRequest, OotdUserDetails user);
 }
