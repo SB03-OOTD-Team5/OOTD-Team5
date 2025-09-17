07f6c8ef97f2e5822665ff064b7868ecad42dd16
Kangmoongu <sprtms5325@naver.com>
2025-09-17 10:37:47 +0900

    feat: 비밀번호 변경 로직 구현

--- src/main/java/com/sprint/ootd5team/domain/user/service/UserService.java
+++ src/main/java/com/sprint/ootd5team/domain/user/service/UserService.java
@@ -3,16 +3,20 @@ package com.sprint.ootd5team.domain.user.service;
 import com.sprint.ootd5team.base.exception.user.UserAlreadyExistException;
 import com.sprint.ootd5team.base.exception.user.UserNotFoundException;
 import com.sprint.ootd5team.domain.user.dto.UserDto;
+import com.sprint.ootd5team.domain.user.dto.request.ChangePasswordRequest;
 import com.sprint.ootd5team.domain.user.dto.request.UserCreateRequest;
 import com.sprint.ootd5team.domain.user.entity.Role;
 import com.sprint.ootd5team.domain.user.entity.User;
 import com.sprint.ootd5team.domain.user.mapper.UserMapper;
 import com.sprint.ootd5team.domain.user.repository.UserRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -38,5 +42,17 @@ public class UserService {
                 Role.USER));
 
         return userMapper.toDto(user);
+    }
+
+    /**
+     * 비밀번호를 바꾸는 메서드
+     * @param userId 바꾸고자 하는 유저의 Id
+     * @param request 바꿀 비밀번호
+     */
+    @Transactional
+    public void changePassword(UUID userId, ChangePasswordRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        user.updatePassword(passwordEncoder.encode(request.password()));
+        userRepository.save(user);
     }
 }
