889999d72699edebd2e1450b1c03d41f1a0f916c
hyohyo-zz <akbkck8101@gmail.com>
2025-09-17 12:01:58 +0900

    fix: 스키마 createdAt 생성으로 base 엔티티 상속

--- src/main/java/com/sprint/ootd5team/domain/clothattribute/entity/ClothesAttributeValue.java
+++ src/main/java/com/sprint/ootd5team/domain/clothattribute/entity/ClothesAttributeValue.java
@@ -1,18 +1,15 @@
 package com.sprint.ootd5team.domain.clothattribute.entity;
 
+import com.sprint.ootd5team.base.entity.BaseEntity;
 import com.sprint.ootd5team.domain.clothes.entity.Clothes;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
-import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -39,12 +36,8 @@ import lombok.Setter;
 )
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ClothesAttributeValue {
+public class ClothesAttributeValue extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.UUID)
-	@Column(columnDefinition = "uuid",updatable = false,nullable = false)
-	UUID id;
 	/**
 	 * 어떤 의상인지
 	 */
