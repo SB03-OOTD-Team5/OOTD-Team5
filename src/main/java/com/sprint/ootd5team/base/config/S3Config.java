af5a8d42eb0cf4876871e4e9104150e33ee8c5cb
hyohyo-zz <akbkck8101@gmail.com>
2025-09-17 12:01:25 +0900

    fix: s3Config storageType이 s3타입일 경우에만 활성화

--- src/main/java/com/sprint/ootd5team/base/config/S3Config.java
+++ src/main/java/com/sprint/ootd5team/base/config/S3Config.java
@@ -1,6 +1,7 @@
 package com.sprint.ootd5team.base.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -10,6 +11,10 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
+@ConditionalOnProperty(
+    name = "ootd.storage.type",
+    havingValue = "s3"
+)
 public class S3Config {
 
     @Value("${ootd.storage.s3.access-key}")
