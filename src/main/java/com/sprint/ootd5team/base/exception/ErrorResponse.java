cc3d62c876a7fa4486e5db16d3464b0ee708ac25
Leichtstar <leichtnidel@gmail.com>
2025-09-18 18:51:58 +0900

    Revert "doc: 의상속성 Swagger API 문서 등록, INVALID_ATTRIBUTE 에러코드 정정"
    This reverts commit 2bde036b30432e58c72e6216ca9a7116bdaa910e.

--- src/main/java/com/sprint/ootd5team/base/exception/ErrorResponse.java
+++ src/main/java/com/sprint/ootd5team/base/exception/ErrorResponse.java
@@ -1,6 +1,5 @@
 package com.sprint.ootd5team.base.exception;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.Getter;
@@ -8,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-@Schema(description = "API 오류 응답", example = "{\n  \"exceptionName\": \"string\",\n  \"message\": \"string\",\n  \"details\": {\n    \"additionalProp1\": \"string\",\n    \"additionalProp2\": \"string\",\n    \"additionalProp3\": \"string\"\n  }\n}")
 public class ErrorResponse {
 
     private final String message;
--- src/main/java/com/sprint/ootd5team/domain/clothattribute/controller/ClothesAttributeController.java
+++ src/main/java/com/sprint/ootd5team/domain/clothattribute/controller/ClothesAttributeController.java
@@ -7,52 +7,67 @@ import com.sprint.ootd5team.domain.clothattribute.service.ClothesAttributeServic
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/clothes/attribute-defs")
 @RequiredArgsConstructor
-@Validated
-public class ClothesAttributeController implements ClothesAttributeApi {
+public class ClothesAttributeController {
 	private final ClothesAttributeService clothesAttributeService;
 
 	/** 속성 등록
 	 * 입력:
 	 * ClothesAttributeDefCreateRequest
 	 */
-	@Override
-	public ClothesAttributeDefDto register(ClothesAttributeDefCreateRequest request){
-		return clothesAttributeService.create(request);
+	@PostMapping
+	public ResponseEntity<ClothesAttributeDefDto> register(
+			@RequestBody ClothesAttributeDefCreateRequest request){
+		ClothesAttributeDefDto result = clothesAttributeService.create(request);
+		return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
 
 	/**
 	 * 의상 속성 정의 목록 조회
 	 */
-	@Override
-	public List<ClothesAttributeDefDto> list(
-		String sortBy,
-		String sortDirection,
-		String keywordLike) {
+	@GetMapping
+	public ResponseEntity<List<ClothesAttributeDefDto>> list(
+		@RequestParam(defaultValue = "createdAt") String sortBy,
+		@RequestParam(defaultValue = "ASCENDING") String sortDirection,
+		@RequestParam(required = false) String keywordLike) {
 
-		return clothesAttributeService.findAll(sortBy, sortDirection, keywordLike);
+		return ResponseEntity.ok(
+			clothesAttributeService.findAll(sortBy, sortDirection, keywordLike)
+		);
 	}
 
 	/**
 	 * 의상 속성 정의 수정
 	 */
-	@Override
-	public ClothesAttributeDefDto update(
-		UUID id, ClothesAttributeDefUpdateRequest request
+	@PatchMapping("/{id}")
+	public ResponseEntity<ClothesAttributeDefDto> update(
+		@PathVariable UUID id, @Validated @RequestBody ClothesAttributeDefUpdateRequest request
 	){
-		return clothesAttributeService.update(id,request);
+		ClothesAttributeDefDto result = clothesAttributeService.update(id,request);
+		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
 	/**
 	 * 의상 속성 정의 삭제
 	 */
-	@Override
-	public void delete(UUID id){
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(
+		@PathVariable UUID id){
 		clothesAttributeService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
