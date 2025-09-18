6e0a23f321b58f8de6babb26055863261c3d165e
yeokyeong <yeokyeong1004@gmail.com>
2025-09-18 10:11:08 +0900

    feat: weather 위치 정보 api 추가

--- src/main/java/com/sprint/ootd5team/domain/weather/controller/WeatherController.java
+++ src/main/java/com/sprint/ootd5team/domain/weather/controller/WeatherController.java
@@ -1,5 +1,7 @@
 package com.sprint.ootd5team.domain.weather.controller;
 
+import com.sprint.ootd5team.domain.location.dto.data.WeatherAPILocationDto;
+import com.sprint.ootd5team.domain.location.service.LocationService;
 import com.sprint.ootd5team.domain.weather.controller.api.WeatherApi;
 import com.sprint.ootd5team.domain.weather.dto.data.WeatherDto;
 import com.sprint.ootd5team.domain.weather.service.WeatherService;
@@ -19,15 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherController implements WeatherApi {
 
     private final WeatherService weatherService;
+    private final LocationService locationService;
 
     @GetMapping
     @Override
-    public ResponseEntity<List<WeatherDto>> getWeatherByLocation(@RequestParam BigDecimal longitude,
-        @RequestParam BigDecimal latitude) {
+    public ResponseEntity<List<WeatherDto>> getWeatherByLocation(@RequestParam BigDecimal latitude,
+        @RequestParam BigDecimal longitude
+    ) {
         try {
-            List<WeatherDto> weatherDtos = weatherService.fetchWeatherByLocation(longitude,
-                latitude);
+            List<WeatherDto> weatherDtos = weatherService.fetchWeatherByLocation(latitude,
+                longitude);
             return ResponseEntity.status(HttpStatus.OK).body(weatherDtos);
+        } catch (Exception e) {
+            //TODO: body에 ErrorResponse 넣기
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @GetMapping("/location")
+    @Override
+    public ResponseEntity<WeatherAPILocationDto> getLocation(@RequestParam BigDecimal latitude,
+        @RequestParam BigDecimal longitude
+    ) {
+        try {
+            WeatherAPILocationDto locationDto = locationService.fetchLocation(latitude,
+                longitude);
+            return ResponseEntity.status(HttpStatus.OK).body(locationDto);
         } catch (Exception e) {
             //TODO: body에 ErrorResponse 넣기
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
