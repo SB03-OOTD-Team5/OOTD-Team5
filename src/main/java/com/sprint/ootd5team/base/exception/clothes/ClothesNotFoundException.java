d02077db13475af83a171d3cd239ae88b331ebdd
Im-Ubin <yuvin3493@gmail.com>
2025-09-18 23:35:32 +0900

    feat: ClothesNotFoundException 생성자 추가

--- src/main/java/com/sprint/ootd5team/base/exception/clothes/ClothesNotFoundException.java
+++ src/main/java/com/sprint/ootd5team/base/exception/clothes/ClothesNotFoundException.java
@@ -1,6 +1,7 @@
 package com.sprint.ootd5team.base.exception.clothes;
 
 import com.sprint.ootd5team.base.errorcode.ErrorCode;
+import java.util.Set;
 import java.util.UUID;
 
 public class ClothesNotFoundException extends ClothesException {
@@ -12,6 +13,12 @@ public class ClothesNotFoundException extends ClothesException {
     public static ClothesNotFoundException withId(UUID clothesId) {
         ClothesNotFoundException exception = new ClothesNotFoundException();
         exception.addDetail("clothesId", clothesId);
+        return exception;
+    }
+
+    public static ClothesNotFoundException withIds(Set<UUID> clothesIds) {
+        ClothesNotFoundException exception = new ClothesNotFoundException();
+        exception.addDetail("clothesIds", clothesIds);
         return exception;
     }
 }
