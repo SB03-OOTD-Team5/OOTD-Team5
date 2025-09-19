379907c162eac5c2f8b0f76e9c2a386199956cd7
yeokyeong <yeokyeong1004@gmail.com>
2025-09-19 16:43:47 +0900

    fix: 프로필 location dto 변경 및 mapper 변경

--- src/main/java/com/sprint/ootd5team/domain/location/mapper/LocationMapper.java
+++ src/main/java/com/sprint/ootd5team/domain/location/mapper/LocationMapper.java
@@ -3,6 +3,7 @@ package com.sprint.ootd5team.domain.location.mapper;
 import com.sprint.ootd5team.domain.location.dto.data.ClientCoords;
 import com.sprint.ootd5team.domain.location.dto.data.WeatherAPILocationDto;
 import com.sprint.ootd5team.domain.location.entity.Location;
+import com.sprint.ootd5team.domain.profile.entity.Profile;
 import com.sprint.ootd5team.domain.weather.entity.Weather;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
@@ -28,6 +29,27 @@ public interface LocationMapper {
             locationNames,
             w.getLatitude(),
             w.getLongitude()
+        );
+    }
+
+    /* profile 내부에서 locationDto로 변경할때 */
+    @Named("profileToLocationDto")
+    default WeatherAPILocationDto profileToLocationDto(Profile profile) {
+        if (profile == null) {
+            return null;
+        }
+        String names = profile.getLocationNames();
+        String[] locationNames =
+            (names == null || names.isBlank()) ? new String[0] : names.split(" ");
+
+        return new WeatherAPILocationDto(
+            profile.getLatitude(),
+            profile.getLongitude(),
+            profile.getXCoord(),
+            profile.getYCoord(),
+            locationNames,
+            profile.getLatitude(),
+            profile.getLongitude()
         );
     }
 
