bff75b6b02f6c97389aac0eb18ec5831cd690387
hyohyo-zz <akbkck8101@gmail.com>
2025-09-24 11:45:30 +0900

    feat: 카프카 publisher, consumer 생성

--- src/main/java/com/sprint/ootd5team/domain/user/repository/UserRepository.java
+++ src/main/java/com/sprint/ootd5team/domain/user/repository/UserRepository.java
@@ -1,13 +1,18 @@
 package com.sprint.ootd5team.domain.user.repository;
 
 import com.sprint.ootd5team.domain.user.entity.User;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    @Query("select u.id from User u")
+    List<UUID> findAllUserIds();
 }
