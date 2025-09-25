302bf05d748b9edf18807461d40adc7cccc2b480
hyohyo-zz <akbkck8101@gmail.com>
2025-09-25 16:49:28 +0900

    feat: NotificationController valid 추가

--- src/main/java/com/sprint/ootd5team/domain/notification/controller/NotificationController.java
+++ src/main/java/com/sprint/ootd5team/domain/notification/controller/NotificationController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
 @Slf4j
+@Validated
 public class NotificationController implements NotificationApi {
 
     private final NotificationService notificationService;
