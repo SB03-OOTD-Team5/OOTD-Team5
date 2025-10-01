08fa3f355b3353d08512f70db1a527280383fa76
Kangmoongu <sprtms5325@naver.com>
2025-10-01 16:19:19 +0900

    feat: application.yaml에 Oauth2 설정 추가 및 Oauth2 로그인을 지원하는 OotdOauth2UserDetails 생성

--- src/main/java/com/sprint/ootd5team/base/security/OotdOauth2UserDetails.java
+++ src/main/java/com/sprint/ootd5team/base/security/OotdOauth2UserDetails.java
@@ -3,6 +3,7 @@ package com.sprint.ootd5team.base.security;
 import com.sprint.ootd5team.domain.user.dto.UserDto;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -10,23 +11,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @EqualsAndHashCode(of = "userDto")
 @Getter
 @RequiredArgsConstructor
-public class OotdSecurityUserDetails implements UserDetails {
+public class OotdOauth2UserDetails implements UserDetails, OAuth2User {
 
     private final UserDto userDto;
-    private final String password;
+    private final Map<String, Object> attributes;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_"+userDto.role().name()));
+        return List.of(new SimpleGrantedAuthority("Role_"+userDto.role().name()));
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return null;
     }
 
     @Override
@@ -34,5 +41,11 @@ public class OotdSecurityUserDetails implements UserDetails {
         return userDto.email();
     }
 
+    @Override
+    public String getName() {
+        return userDto.name();
+    }
+
     public UUID getUserId() { return userDto.id(); }
+
 }
