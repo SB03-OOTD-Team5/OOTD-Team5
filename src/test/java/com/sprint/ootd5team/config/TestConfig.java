efd7bcf9eb741aa8a1d05556b7a3521c6f60318b
Im-Ubin <yuvin3493@gmail.com>
2025-10-17 13:37:34 +0900

    test: 테스트용 ElasticsearchOperations 빈 등록

--- src/test/java/com/sprint/ootd5team/config/TestConfig.java
+++ src/test/java/com/sprint/ootd5team/config/TestConfig.java
@@ -5,6 +5,7 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 
 @TestConfiguration
 public class TestConfig {
@@ -13,5 +14,11 @@ public class TestConfig {
     @Primary
     public CacheEvictHelper cacheEvictHelper() {
         return Mockito.mock(CacheEvictHelper.class);
+    }
+
+    @Bean
+    @Primary
+    public ElasticsearchOperations elasticsearchOperations() {
+        return Mockito.mock(ElasticsearchOperations.class);
     }
 }
