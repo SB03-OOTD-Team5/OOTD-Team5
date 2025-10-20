32456487551ee9732f187ec99821791ea0b21ed6
hyohyo-zz <akbkck8101@gmail.com>
2025-10-20 17:01:10 +0900

    test: s3 testConfig 추가, 프로필 분리

--- src/test/java/com/sprint/ootd5team/config/TestConfig.java
+++ src/test/java/com/sprint/ootd5team/config/TestConfig.java
@@ -1,10 +1,14 @@
 package com.sprint.ootd5team.config;
 
+import static org.mockito.Mockito.mock;
+
 import com.sprint.ootd5team.base.cache.CacheEvictHelper;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @TestConfiguration
 public class TestConfig {
@@ -12,6 +16,16 @@ public class TestConfig {
     @Bean
     @Primary
     public CacheEvictHelper cacheEvictHelper() {
-        return Mockito.mock(CacheEvictHelper.class);
+        return mock(CacheEvictHelper.class);
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return mock(S3Client.class);
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return mock(S3Presigner.class);
     }
 }
